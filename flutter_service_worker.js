cb06c97e03506dbb306bd723466c4d4f32adef01
thanweer m <thanweer@wiinnova.com>
2025-10-30 15:08:21 +0530

    Add web build for hosting

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e7cddd368a3b90ea8d7aee4dada3c383
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "6ab0b2dccea6baf08a0f4d6033b36562",
+"flutter_bootstrap.js": "0d23230611dda4717d0151bd542ff31f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
